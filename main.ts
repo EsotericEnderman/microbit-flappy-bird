2d582a00ffd99bca66b2e70283a426a455d9fa96
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-01 04:45:09 +0100

    Rework pipe spawning

--- main.ts
+++ main.ts
@@ -15,11 +15,13 @@ let update = () => {
 let spawnPipe = () => {
     const squareLocations: Square[] = [new Square(4, 0), new Square(4, 1), new Square(4, 2), new Square(4, 3), new Square(4, 4)];
 
+    let indexToRemove = Math.floor(Math.random() * squareLocations.length);
+    let direction = Math.random() >= 0.5 ? 1 : -1;
     let squaresToRemove = 1 + Math.floor(Math.random() * 4);
 
-    while (squaresToRemove != 0) {
-        const squareToRemoveIndex = Math.floor(Math.random() * squareLocations.length);
-        squareLocations.splice(squareToRemoveIndex, 1);
+    while (squaresToRemove != 0 && indexToRemove >= 0 && indexToRemove < squareLocations.length) {
+        indexToRemove+=direction;
+        squareLocations.splice(indexToRemove, 1);
 
         squaresToRemove--;
     }
